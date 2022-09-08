c03cf9bf277b4208442178b18ca34703118badee
khosta77 <puwerfulpants@gmail.com>
2022-09-08 09:49:06 +0300

    тестовые задания

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-
+    long long A, B;
+    ifstream is("input.txt");
+    is >> A >> B;
+    is.close();
+    std::ofstream out;          // поток для записи
+    out.open("output.txt"); // окрываем файл для записи
+    out << (A + B) << endl;
+    out.close();
     return 0;
 }
