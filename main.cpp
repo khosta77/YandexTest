d8c11715f8c1edf9f6b91b9d4ef5881e4d259f4e
khosta77 <puwerfulpants@gmail.com>
2022-09-10 19:20:58 +0300

    t14

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <string>
-#include <set>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    string J, S;
-    cin >> J >> S;
-    set<char> st;
-    for (size_t i = 0; i < J.size(); i++) {
-        st.insert(J[i]);
+    string A, B, C;
+    cin >> A >> B;
+    const size_t AN = A.size();
+    C.resize((1 + AN), ' ');
+    for (size_t i = 0; i < AN; i++) {
+        if (A[i] == B[i]) {
+            C[i] = 'P';
+            A[i] = ' ';
+        } else {
+
+        }
     }
-    int count = 0;
-    for (auto it = st.begin(); it != st.end(); it++) {
-        for (size_t j = 0; j < S.size(); j++) {
-            if (*it == char(S[j])) {
-                count++;
+    for (size_t i = 0; i < AN; i++) {
+        if (C[i] == ' ') {
+            for (size_t j = 0; j < AN; j++) {
+                if (B[i] == A[j]) {
+                    C[i] = 'S';
+                    A[j] = ' ';
+                }
+            }
+
+            if (C[i] == ' ') {
+                C[i] = 'I';
             }
         }
     }
-    cout << count << endl;
+    cout << C << endl;
     return 0;
 }
--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 g++ main.cpp -o main
 #./main
 
-echo "ab" "aabbccd" | ./main
+echo "CLOUD" "CUPID" | ./main
+echo "ALICE" "ELIBO" | ./main
+echo "ABCBCYA" "ZBBACAA" | ./main #IPSSPIP
